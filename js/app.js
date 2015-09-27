2e99180bb8d01832be0b2c2a76015dad2ea9bc8b
sk <sk.valshk@gmail.com>
2015-09-26 22:04:44 -0500

    Use kraken/streams/ endpoint

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
-var twitchLayoutApp = angular.module('twitchLayoutApp', []);
+var twitchLayoutApp = angular.module('twitchLayoutApp', ['angularMoment']);
+
 
 twitchLayoutApp.controller('DashboardCtrl', ['$scope', '$http', '$interval',
         function ($scope, $http, $interval) { 
+            twitchLayoutApp.run(function(amMoment) {
+                amMoment.changeLocale('es');
+            });
+
             $scope.game = '';
-            $scope.status = '';
+            $scope.status = 'Iniciando stream...';
+            $scope.timeup = new Date();
 
             $scope.getChannel = function(username) {
-                var url = 'https://api.twitch.tv/kraken/channels/' + username;
+                var url = 'https://api.twitch.tv/kraken/streams/' + username;
                 $http.get(url).then($scope.setInfo);
             };
 
             $scope.setInfo = function(response) {
-                $scope.game = response.data.game;
-                $scope.status = response.data.status;
+                if (response.data.stream) {
+                    $scope.game = response.data.stream.channel.game;
+                    $scope.status = response.data.stream.channel.status;
+                    $scope.timeup = response.data.stream.created_at;
+                }
             };
 
             $scope.getChannel('eseca');
             $interval($scope.getChannel, 30000, 0, true, ['eseca']);
         }]);
+
